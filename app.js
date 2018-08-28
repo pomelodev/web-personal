9d497eafaa7b95b871ef94f4280c72ca7151f1a1
santiagobeloqui <beloquisantiago@gmail.com>
2018-08-28 11:50:15 -0300

    Habilitar CORS

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 let sendMail = require("./mail");
 
 // app.use(helmet());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
